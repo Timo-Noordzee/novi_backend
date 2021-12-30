df17db66e96c87d6f5fe36c554e4d86eda86bdf7
Timo Noordzee <timo.noordzee@d2t.nl>
2021-12-30 15:23:00 +0100

    Setup project using spring initializr

---
+++ src/test/java/com/timo_noordzee/novi/backend/GarageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timo_noordzee.novi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
